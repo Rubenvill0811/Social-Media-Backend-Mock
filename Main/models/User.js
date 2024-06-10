c3ef63938e19af88b6872d44e62197da297e2f93
Rubenvill0811 <rubenvill0811@gmail.com>
2024-06-09 18:24:17 -0700

    Added a User schema

---
+++ Main/models/User.js
@@ -0,0 +1,50 @@
+const {Schema, Types} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            index: true,
+            validate: {
+                validator: function(v) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid email address!`
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId, 
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
